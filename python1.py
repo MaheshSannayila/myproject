d14725c589e0d1ec8d2753b339cf0b4f3bc80886
MaheshSannayila <140686053+MaheshSannayila@users.noreply.github.com>
2023-09-01 12:28:58 +0530

    Add files via upload

--- python1.py
+++ python1.py
@@ -1,85 +1,70 @@
-# 1.Write a Python script to add a key to a dictionary.
-# Sample Dictionary : {0: 10, 1: 20}
-# Expected Result : {0: 10, 1: 20, 2: 30}
+# count Upper case and lower case
+def find_num_of_uppercase_and_lowercase(a):
+    upper_count=0
+    lower_count=0
+    for i in a:
+        if i.isupper():
+            upper_count+=1
+        else:
+            lower_count+=1
+    print(upper_count)
+    print(lower_count)
 
-a={0: 10, 1: 20}
-a[2]=30
-print(a)
+a=input()
+find_num_of_uppercase_and_lowercase(a)
 
+# get a unique list
 
-# 2.Write a Python script to check whether a given key already exists in a dictionary.
-a={"mahesh":"2","suresh":"5","girish":"35"}
-b=input()
-keys=list(a)
-if b in keys:
-    print("alredy exists")
-else:
-    print("key not in a dictionary")
+def get_unique_list(a):
+    unique_list=[]
+    for i in a:
+        if i not in unique_list:
+            unique_list.append(i)
+    print(unique_list)
 
-# 3.Write a Python program to iterate over dictionaries using for loops
+a=[1,2,3,3,3,3,4,5]
+get_unique_list(a)
 
-a={"mahesh":"2","suresh":"5","girish":"35"}
-for i in a.items():
-    print(i)
+# sum all the numbers in a list.
 
-# 4.Write a Python script to print a dictionary where the keys are numbers between 1 and 15 (both included) and the values are the square of the keys.
-# Sample Dictionary
-dict_a={}
-for i in range(1,15+1):
-    dict_a[i]=(i**2)
-print(dict_a)
+def get_sum(a):
+    sum=0
+    for i in a:
+        sum+=i
+    print(sum)
+a=[8, 2, 3, 0, 7]
+get_sum(a)
 
-# 5.Write a Python program to create a dictionary from a string.
-# Expected output: {'m': 1, 'a': 1, 'o': 3, 'l': 2, 'i': 1, 'x': 1, 't': 1, 'e': 1,'c': 1, 'h': 1, 'n': 1, 'g': 1,'y':1}
-string_a="marolix technology"
-string_a=string_a.replace(" ","")
-conve_list=list(string_a)
-reversed_list=conve_list[::-1]
-for i in reversed_list:
-    if (reversed_list.count(i))>1:
-        reversed_list.remove(i)
-duplicat_removed_list=reversed_list[::-1]
-result={}
-for i in duplicat_removed_list:
-    count_1=conve_list.count(i)
-    result[i]=count_1
-print(result)
-    
-# 6 Write a Python program to sum all the items in a dictionary.
-a={'m': 1, 'a': 1, 'o': 3, 'l': 2, 'i': 1, 'x': 1, 't': 1, 'e': 1,'c': 1, 'h': 1, 'n': 1, 'g': 1,'y':1}
-len_a=len(a)
-print(len_a)
+# pangram r not
 
-# 7.Write a Python program to combine two dictionary by adding values for common keys.
-d1 = {'a': 100, 'b': 200, 'c':300}
-d2 = {'a': 300, 'b': 200, 'd':400}
-list_keys=list(d2)
-d3={}
-for i,j in d1.items():
-    if i in list_keys:
-        m=d2.get(i)
+def find_pangram_r_not(a):
+    unique_set=(set(a))
+    if len(unique_set)==26:
+        print("its pangram")
     else:
-        m=0
-    d3[i]=m+j
-# print(d3)
-for i,j in d2.items():
-    if(i not in d1):
-        d3[i]=j
-print(d3)
+        print("not pangram")
 
-# 8.Write a Python program to access dictionary key's element by index.
-# Expected Output:
-# physics
-# math
-# chemistry
+a=input()
+a=a.replace(" ","")
+find_pangram_r_not(a)
 
-# 9.Write a Python program to remove a key from a dictionary
+# print a list where the values are the squares of numbers between 1 and 10 (both included).
 
-d1 = {'a': 100, 'b': 200, 'c':300}
-print(d1.pop("a"))
-print(d1)
+def get_nums_sqrs_list(a,b):
+    list_1=[]
+    for i in range(a,b):
+        list_1.append(i**2)
+    print(list_1)
+a=int(input())
+b=int(input())
+get_nums_sqrs_list(a,b+1)
 
-# 10.Write a Python script to merge two Python dictionaries.d
-d1 = {'a': 100, 'b': 200, 'c':300}
-d2 = {'g': 300, 'e': 200, 'd':400}
-print(d1|d2)
+# find sum of given values, pass values has variable number of arguments to function.
+
+def get_sum_args(*a):
+    sum=0
+    for i in a:
+        sum+=i
+    print(sum)
+
+get_sum_args(1,2,3,4,5)
