1cc95bf6d4ed3c47caf736133c09ae65b3822cfb
MaheshSannayila <140686053+MaheshSannayila@users.noreply.github.com>
2023-11-21 19:09:59 +0530

    Add files via upload

--- hosipital.py
+++ hosipital.py
@@ -9,22 +9,34 @@ class hosipital():
     def __init__(self):
         self.patient_details={}
 
-    def add_patient_details(self,p_name,p_id,p_diseases,op_no):
+    def add_patient_details(self):
+        p_name=input("enter_patient_name :")
+        p_id =input("enther_empolye_id :")
+        p_diseases=input("enter patien diseases :")
+        op_no=int(input("enter_op_num :"))
         self.patient_details[p_id]={"patient_name":p_name,"p_id":p_id,"p_diseases":p_diseases,"op_no":op_no}
 
-    def display_patient_details(self):
+    def display_all_patient_details(self):
         print(self.patient_details)
 
-    def update_patient_details(self,p_name,p_id,p_diseases,op_no):
+    def update_patient_details(self):
+        p_name=input("enter_patient_name :")
+        p_id =input("enther_empolye_id :")
+        p_diseases=input("enter patien diseases :")
+        op_no=int(input("enter_op_num :"))
         self.patient_details[p_id]={"patient_name":p_name,"p_id":p_id,"p_diseases":p_diseases,"op_no":op_no}
     
-    def delete_particular_method(self,p_id):
+    def delete_particular_method(self):
+        p_id=input("enter_patient_id :")
         del self.patient_details[p_id]
 
     def get_particular_patient_details(self,p_id):
+        p_id=input("enter_patient_id :")
+
         print(self.patient_details[p_id])
 
-    def with_one_details_of_patient_get_all_details_of_patient(self,p_details):
+    def display_particulr_patient_details(self):
+        p_details=input("enter_one_of patient_details :")
         for i in self.patient_details:
             self.values=list(self.patient_details[i].values())
             # print(self.values)
@@ -32,8 +44,9 @@ class hosipital():
                 print(self.patient_details[i])
 
 h_obj=hosipital()
-h_obj.add_patient_details("ganesh",233,"fivar",1)
-h_obj.add_patient_details("suresh",234,"fivar",2)
-h_obj.update_patient_details("suresh",234,"dengue",3)
-# h_obj.display_patient_details()
-h_obj.with_one_details_of_patient_get_all_details_of_patient("dengue")
+h_obj.add_patient_details()
+# h_obj.add_patient_details()
+
+h_obj.display_all_patient_details()
+h_obj.display_particulr_patient_details()
+
